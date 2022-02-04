820baf671bae9735412d46b2432529904ec2e027
Yuri Mascarenhas <dev.yurimascarenhas@gmail.com>
2022-02-03 22:29:37 -0300

    exercicios 1, 2 e 3 - dia 4.2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-1.js
@@ -0,0 +1,11 @@
+/* Leia atentamente os enunciados e faça o que se pede! Iremos utilizar esse array para realizar os exercícios do 1 ao 7:
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log(); */
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for(let i of numbers) {
+  console.log(i);
+}
