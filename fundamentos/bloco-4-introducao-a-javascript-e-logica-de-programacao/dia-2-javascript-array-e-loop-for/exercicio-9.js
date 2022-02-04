37bb04527946f1d08762e1c88d5172339ce319b0
Yuri Mascarenhas <dev.yurimascarenhas@gmail.com>
2022-02-03 23:46:47 -0300

    exercicios 8 e 9 - dia 4.2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-9.js
@@ -0,0 +1,8 @@
+// Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
+
+let exercicio = []
+
+for (let i = 0; i < 25; i += 1) {
+  exercicio.push(i + 1);
+  console.log(exercicio[i] / 2);
+}
