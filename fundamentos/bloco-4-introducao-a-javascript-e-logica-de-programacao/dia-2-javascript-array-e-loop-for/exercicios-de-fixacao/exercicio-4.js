956618d6b319c6cfb2993b88cc07c841a6a9c5ae
Yuri Mascarenhas <dev.yurimascarenhas@gmail.com>
2022-02-03 22:09:56 -0300

    exercicios de fixacao 4 e 5 - dia 4.2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-de-fixacao/exercicio-4.js
@@ -0,0 +1,9 @@
+/* Utilize o for para imprimir os elementos da lista groceryList com o console.log() :
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia']; */
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for(let i = 0; i < groceryList.length; i += 1) {
+  console.log(groceryList[i]);
+}
