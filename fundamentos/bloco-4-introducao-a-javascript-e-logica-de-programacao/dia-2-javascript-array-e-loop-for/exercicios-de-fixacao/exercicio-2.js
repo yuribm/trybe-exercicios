51a0c2847e3b473526b5cdd9636b6efd709ac8b2
Yuri Mascarenhas <dev.yurimascarenhas@gmail.com>
2022-02-03 21:17:59 -0300

    atualizacao exercicios de fixacao 1, 2 e 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-de-fixacao/exercicio-2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-de-fixacao/exercicio-2.js
@@ -1,3 +1,10 @@
+/*Procure o índice do valor "Portfólio" do array menu :
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = ;
+
+console.log(indexOfPortfolio);*/
+
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let indexOfPortfolio = menu.indexOf('Portfólio');
 
--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-de-fixacao/exercicio-3.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-de-fixacao/exercicio-3.js
@@ -1,3 +1,9 @@
+/*Adicione o valor "Contato" no final do array menu :
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+console.log(menu);*/
+
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 
 menu.push('Contato');
