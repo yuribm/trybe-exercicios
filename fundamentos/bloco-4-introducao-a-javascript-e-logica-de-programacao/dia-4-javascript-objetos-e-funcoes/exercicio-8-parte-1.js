d5bd1982828323b054c16172bb4cb9fd38a425dc
Yuri Mascarenhas <dev.yurimascarenhas@gmail.com>
2022-02-08 00:06:20 -0300

    exercicios 7 e 8 - parte 1 - dia 4.4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio-8-parte-1.js
@@ -0,0 +1,25 @@
+/* para este exercício será utilizado o resultado do anterior.
+
+8 - Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: "Julia tem 2 livros favoritos".
+*/
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+
+leitor.livrosFavoritos[1] = {
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editora: 'Rocco',
+}
+
+console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' favoritos')
