a6fc60a3159a9be695058f22d55bd3ac7440e927
Yuri Mascarenhas <dev.yurimascarenhas@gmail.com>
2022-04-09 16:27:07 -0300

    exercicio-de-fixacao-1 dia-10/2

---
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/exercicio-de-fixacao-1/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import Image from './Image';
+import './App.css';
+
+function App() {
+  return (
+    <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='cute cat staring' />
+  );
+}
+
+export default App;
