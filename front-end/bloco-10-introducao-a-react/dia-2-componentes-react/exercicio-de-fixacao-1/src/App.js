f84485b546e4cbdaa8608bc76b6d1be95ad211d0
Yuri Mascarenhas <dev.yurimascarenhas@gmail.com>
2022-04-10 00:17:12 -0300

    exercicio de fixacao 1 - dia 10/2 - refatorado

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/exercicio-de-fixacao-1/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/exercicio-de-fixacao-1/src/App.js
@@ -3,8 +3,11 @@ import Image from './Image';
 import './App.css';
 
 function App() {
+
+  const url = 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg'
+
   return (
-    <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='cute cat staring' />
+    <Image source={url} alternativeText='cute cat staring' />
   );
 }
 
