adee776fe4e9793fcf3628d2dd48bc348676f74e
Yuri Mascarenhas <dev.yurimascarenhas@gmail.com>
2022-04-13 23:38:12 -0300

    exercicio 1 dia 10-2

---
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/exercicio-1/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Pokedex from './components/Pokedex';
+
+function App() {
+  return (
+    <>
+    <header>Pokedex</header>
+    <main>
+      <Pokedex />
+    </main>
+    </>
+  );
+}
+
+export default App;
